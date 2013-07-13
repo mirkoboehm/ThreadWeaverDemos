03d3fc9e2614d7eacd8b4cf716dd698584975d65
Mirko Boehm (AWS) <mirko@agile-workers.com>
2013-07-13 13:50:37 +0200

    Formatting.

--- HelloWorld/HelloWorldRaw.cpp
+++ HelloWorld/HelloWorldRaw.cpp
@@ -9,9 +9,7 @@ using namespace ThreadWeaver;
 int main(int argc, char** argv)
 {
     QCoreApplication app(argc, argv);
-    Lambda l( []() {
-        qDebug() << "Hello World!";
-    } );
+    Lambda l( []() { qDebug() << "Hello World!"; } );
     Weaver::instance()->enqueueRaw(&l);
     Weaver::instance()->finish();
 }
