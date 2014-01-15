11c10983ddfb18b9ff2c411211d1ccd1ce1afffd
Mirko Boehm (Endocode) <mirko@endocode.com>
2014-01-15 22:52:34 +0100

    Explain more concepts in HelloWorldRaw.
    ... at the expense of being more verbose.

--- HelloWorldRaw/HelloWorldRaw.cpp
+++ HelloWorldRaw/HelloWorldRaw.cpp
@@ -3,7 +3,13 @@
 
 int main(int argc, char** argv)
 {
-    ThreadWeaver::Lambda<void(*)()> job( []() { qDebug() << "Hello World!"; } );
+    using namespace ThreadWeaver;
+
     QCoreApplication app(argc, argv);
-    ThreadWeaver::stream() << job;
+    // Allocate a Job as a local variable:
+    Lambda<void(*)()> job( []() { qDebug() << "Hello World!"; } );
+    // Queue the Job using the default Queue stream:
+    stream() << job;
+    // Wait for finish(), because job is destroyed before the global queue:
+    Queue::instance()->finish();
 }
